e7b7e0c1e51f9f7c215e76b169856db1a0d211d3
Fayez Anwar <81994166+pnzrdlr17@users.noreply.github.com>
2023-11-22 17:53:40 +0530

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -16,14 +16,14 @@ function ProfilePage(props) {
 export async function getServerSideProps(context) {
   const session = await getSession({ req: context.req });
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      },
-    };
-  }
+  // if (!session) {
+  //   return {
+  //     redirect: {
+  //       destination: '/auth',
+  //       permanent: false,
+  //     },
+  //   };
+  // }
 
   return {
     props: { session },
